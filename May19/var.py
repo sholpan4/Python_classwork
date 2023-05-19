0cb78e8478a234c9071b4ff84c564704c0949b7a
sholpan4 <b.o.sholpan44@gmail.com>
2023-05-19 21:51:09 +0600

    qwerty
    2 task done

--- May19/var.py
+++ May19/var.py
@@ -1,2 +1,16 @@
-name = "Sholpan"
-print (name)
+# task 1
+user_input = input("Enter number: ")
+user_int = int(user_input)
+
+result = user_int ** 2
+print("Result is", result)
+
+# task 2
+user_input = input("Enter number: ")
+user_int = int(user_input)
+
+user_input = input("Enter number: ")
+user_int = int(user_input)
+
+result = (user_int1 + user_int2) /2
+print("Result is", result)
