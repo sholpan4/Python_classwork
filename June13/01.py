1f777fb15592bbd13b6451bc72fe99fe4c4addc6
sholpan4 <b.o.sholpan44@gmail.com>
2023-06-13 18:48:42 +0600

    june13
    june13

---
+++ June13/01.py
@@ -0,0 +1,9 @@
+list_hor = ["a", "b", "c", "d", "e", "f", "g", "h"]
+list_ver = ["#", " ", "#", " ", "#", " ", "#", " "]
+list_ver2 = [" ", "#", " ", "#", " ", "#", " ", "#"]
+
+for a in list_hor:
+    for b in list_ver:
+        for c in list_ver2:
+        #result = 
+            print(a, b, c)
