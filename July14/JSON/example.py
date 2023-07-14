0132cbdeef698ab230bb2c0f407c367aedd7a99f
S <52811487+sholpan4@users.noreply.github.com>
2023-07-14 19:23:15 +0600

    14
    14

--- July14/JSON/example.py
+++ July14/JSON/example.py
@@ -1,7 +1,9 @@
 import json
 
 data = '{"type": "forecast", "duration": 3, "city": "Karaganda", "country_code": "kz"}'
-
+#обязательно " " для json data
 request = json.loads(data)
+request['city'] = "Almaty"
 
-print(request["city"])
+data = json.dumps(request) #data = json.dumps(request, ensure_ascii=False)
+print(data)
