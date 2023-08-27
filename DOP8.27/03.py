2f0d6b32b8e419277d431afa48d311b6290031f6
sholpan4 <b.o.sholpan44@gmail.com>
2023-08-27 13:57:15 +0600

    27
    27

--- DOP8.27/03.py
+++ DOP8.27/03.py
@@ -26,3 +26,20 @@ print(result)
 out = open('out.json', 'w')
 json.dump(result, out)
 out.close()  
+
+
+print(info['soldiers'][3]['height'])
+print(info['soldiers'][5]['first_name'], info['soldiers'][5]['last_name'], info['soldiers'][5]['height'])
+
+size_list = []
+for value in info['sizes'].values():
+    size_list.append(str(value['to']))   
+
+print(size_list)
+a = " ".join(size_list)    
+print(a)
+
+str(item['to'])
+
+b_list = map(lambda x: x in , info['sizes'].values())
+print(" ".join(b_list))
