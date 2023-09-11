2800b370d46b3b9a4fbb4b40c98c76a8ffad38cd
S <52811487+sholpan4@users.noreply.github.com>
2023-09-11 21:23:52 +0600

    11
    11

--- September11/client.py
+++ September11/client.py
@@ -2,7 +2,7 @@ import socket
 
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-server_address = ('localhost', 9900)
+server_address = ('192.168.110.119', 9900)  #192.168.110.119
 
 message = 'Hello by UDP!'
 my_socket.sendto(message.encode(), server_address)
--- September11/server.py
+++ September11/server.py
@@ -5,5 +5,10 @@ my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 my_address = ('localhost', 9900)
 my_socket.bind(my_address)
 
-data, addr = my_socket.recvfrom(1024)
-print(f'Получено сообщение от {addr}: {data.decode(encoding="utf-8")}')
+is_running = True
+while is_running:
+    data, addr = my_socket.recvfrom(1024)
+    message = data.decode(encoding="utf-8")
+    print(f'Получено сообщение от {addr}: {message}')
+    if message == "exit":
+        is_running = False
