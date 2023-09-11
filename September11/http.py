5df8c8b80330cad2572ffd30deb1dda7cdca0345
S <52811487+sholpan4@users.noreply.github.com>
2023-09-11 21:49:06 +0600

    11
    11

---
+++ September11/http.py
@@ -0,0 +1,21 @@
+import socket
+
+my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+my_address = ('localhost', 3000)
+my_socket.bind(my_address)
+
+my_socket.listen()
+
+is_running = True
+while is_running:
+    client_socket, addr = my_socket.accept()
+    data = client_socket.recv(1024)
+
+    message = data.decode(encoding="utf-8")
+    print(f'Получено сообщение от {addr}: {message}')
+
+    client_socket.send("HTTP/1.1\n")
+
+    if message == "exit":
+        is_running = False
