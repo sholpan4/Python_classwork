44e5b1e230c4c714712b978a9684c0c9b18f774c
S <52811487+sholpan4@users.noreply.github.com>
2023-09-12 18:54:17 +0600

    1
    1

--- September11/client.py
+++ September11/client.py
@@ -2,7 +2,7 @@ import socket
 
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-server_address = ('192.168.110.119', 9900)  #192.168.110.119
+server_address = ('localhost', 9900)  #192.168.110.119  127.0.0.1
 
-message = 'Hello by UDP!'
+message = 'Sholpan'
 my_socket.sendto(message.encode(), server_address)
