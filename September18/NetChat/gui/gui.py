75d43c6df25120d87d17c8284fc5a47492b33b3c
S <52811487+sholpan4@users.noreply.github.com>
2023-09-25 21:46:33 +0600

    `
    `

--- September18/NetChat/gui/gui.py
+++ September18/NetChat/gui/gui.py
@@ -27,12 +27,15 @@ class Gui(QObject):
     def set_window(self, window_name, username=None):
         if self.window is not None:
             self.window.hide()
-        if window_name == 'MainWindow':
-            self.window = MainWindow(username)
-            self.window.sendMessage.connect(self.sendMessage)
-        elif window_name == 'LoginWindow':
-            self.window = LoginWindow()
-            self.window.loginUser.connect(self.loginUser)
+        match window_name:
+            case 'MainWindow':
+                self.window = MainWindow(username)
+                self.window.sendMessage.connect(self.sendMessage)
+            case 'LoginWindow':
+                self.window = LoginWindow()
+                self.window.loginUser.connect(self.loginUser)
+            case _:
+                log.e('ERROR', window_name)
         if self.running:
             self.run()
         
