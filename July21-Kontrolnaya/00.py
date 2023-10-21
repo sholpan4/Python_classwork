6bc1f2c19c7a035d2163c9e5d8040ea1ddd44b90
sholpan4 <b.o.sholpan44@gmail.com>
2023-10-21 21:45:35 +0600

    `
    `

--- July21-Kontrolnaya/00.py
+++
@@ -1,4 +0,0 @@
-def distance(point1, point2):
-    a2 = (point1.x - point2.x) ** 2
-    b2 = (point1.y - point2.y) ** 2
-    return (a2 + b2) ** 0.5
--- July21-Kontrolnaya/draft.py
+++
@@ -1,4 +0,0 @@
-input = [21, 9, 6, 11, 4, 15]
-
-num = input[::-1]
-print(num)
